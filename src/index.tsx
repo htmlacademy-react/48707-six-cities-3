e9d4e78c92810649bccef004d17b50d003f85495
Марченко Виктория <m.n.i.mo@mail.ru>
2025-08-19 17:07:25 +0600

    correct

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/App/App.tsx';
+import App from './components/App/App';
 import {Setting} from './const.tsx';
 
 const root = ReactDOM.createRoot(
