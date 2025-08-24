e779842f8ea1aa37480d7147395c7cea42eeb90a
Марченко Виктория <m.n.i.mo@mail.ru>
2025-08-24 15:15:49 +0600

    module3-task1

---
+++ src/pages/NotFound/NotFound.tsx
@@ -0,0 +1,9 @@
+function NotFound(): JSX.Element {
+  return (
+    <div className="page page--gray">
+      <h1>Ошибка 404. Страница не существует.</h1>
+    </div>
+  );
+}
+
+export default NotFound;
