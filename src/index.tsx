4d1ae23370b885b4f23922ec6ca2b9981e31d210
Марченко Виктория <m.n.i.mo@mail.ru>
2025-09-04 20:56:19 +0600

    module4-task1

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App.tsx';
-import {Setting} from './const.tsx';
+import { Setting } from './const.tsx';
+import { offers } from './mocks/offers';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -11,6 +12,7 @@ root.render(
   <React.StrictMode>
     <App
       numberOfRentalOffers = {Setting.numberOfRentalOffers}
+      offers = {offers}
     />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
